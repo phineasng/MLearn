7cb36015c50941583a231cc1ff28fc7bdce7b43e
phineasng <nguyen.phineas@gmail.com>
2015-12-12 13:19:33 +0100

    Modified some macros

--- Base/MLearnMacros.h
+++ Base/MLearnMacros.h
@@ -17,38 +17,49 @@
 #define __func__ __FUNCTION__
 #endif
 
+
 // DEBUG MACROS
 #ifndef NDEBUG
 
 // ASSERT
 #define MLEARN_ASSERT(x,msg)\
 	if (!(x)){\
-		std::cerr << "ERROR in '" << __func__ << "()': " << msg << std::endl;\
-		std::cerr << "\tAssert failed: " <<  #x << std::endl;\
+		std::cerr << "ERROR in '" << __func__ << "()': " << msg << "\n";\
+		std::cerr << "\tAssert failed: " <<  #x << "\n";\
 		throw( std::runtime_error( "An error occured. Check your error stream!" ) );\
 	}
 
 // WARNING
 #define MLEARN_WARNING(x,msg)\
 	if (!(x)){\
-		std::cerr << "WARNING in '" << __func__ << "()': " << msg << std::endl;\
-		std::cerr << "\tCheck failed: " <<  #x << std::endl;\
+		std::cerr << "WARNING in '" << __func__ << "()': " << msg << "\n";\
+		std::cerr << "\tCheck failed: " <<  #x << "\n";\
 	}
 
-// WARNING MESSAGE
+// ERROR MESSAGE
 #define MLEARN_ERROR_MESSAGE(msg)\
 	{\
-		std::cerr << "ERROR in '" << __func__ << "()': " << msg << std::endl;\
+		std::cerr << "ERROR in '" << __func__ << "()': " << msg << "\n";\
 		throw( std::runtime_error( "An error occured. Check your error stream!" ) );\
 	}
 
 // WARNING MESSAGE
-#define MLEARN_WARNING_MESSAGE(msg) { std::cerr << "WARNING in '" << __func__ << "()': " << msg << std::endl; }
+#define MLEARN_WARNING_MESSAGE(msg) { std::cerr << "WARNING in '" << __func__ << "()': " << msg << "\n"; }
 
 #else
 #define MLEARN_ASSERT(x,msg)
 #define MLEARN_WARNING(x,msg)
+#define MLEARN_ERROR_MESSAGE(msg)
 #define MLEARN_WARNING_MESSAGE(msg)
 #endif // NDEBUG
+
+// FORCED ERROR MESSAGE
+#define MLEARN_FORCED_ERROR_MESSAGE(msg)\
+	{\
+		std::cerr << "ERROR in '" << __func__ << "()': " << msg << "\n";\
+		throw( std::runtime_error( "An error occured. Check your error stream!" ) );\
+	}
+// FORCED WARNING MESSAGE
+#define MLEARN_FORCED_WARNING_MESSAGE(msg) { std::cerr << "WARNING in '" << __func__ << "()': " << msg << "\n"; }
 
 #endif 

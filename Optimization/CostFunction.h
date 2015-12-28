82022a5b9483c890b2f7f3f072166b8d112564b2
phineasng <nguyen.phineas@gmail.com>
2015-12-28 22:46:12 +0100

    Fixed little bug

--- Optimization/CostFunction.h
+++ Optimization/CostFunction.h
@@ -9,31 +9,31 @@
 
 // USEFUL MACRO FOR DEFINING NECESSARY MEMBER FUNCTIONS
 #define TEMPLATED_SIGNATURE_EVAL_FUNCTION(x_variable)\
-	template < 	typename DERIVED,\
-				typename = typename std::enable_if< DERIVED::ColsAtCompileTime == 1 , DERIVED >::type >\
-	typename DERIVED::Scalar eval( const Eigen::MatrixBase<DERIVED>& x_variable ) const
+	template < 	typename DERIVED_SIGNATURE_MACRO_q1w2,\
+				typename = typename std::enable_if< DERIVED_SIGNATURE_MACRO_q1w2::ColsAtCompileTime == 1 , DERIVED_SIGNATURE_MACRO_q1w2 >::type >\
+	typename DERIVED_SIGNATURE_MACRO_q1w2::Scalar eval( const Eigen::MatrixBase<DERIVED_SIGNATURE_MACRO_q1w2>& x_variable ) const
 
 
 #define TEMPLATED_SIGNATURE_ANALYTICAL_GRADIENT_FUNCTION(x_variable,gradient_to_compute)\
-	template< 	typename DERIVED,\
-		   		typename DERIVED_2,\
-		   		typename = typename std::enable_if< DERIVED::ColsAtCompileTime == 1 , DERIVED >::type,\
-		   		typename = typename std::enable_if< DERIVED_2::ColsAtCompileTime == 1 , DERIVED_2 >::type,\
-				typename = typename std::enable_if< std::is_floating_point<typename DERIVED::Scalar>::value , typename DERIVED::Scalar >::type,\
-				typename = typename std::enable_if< std::is_same<typename DERIVED::Scalar, typename DERIVED_2::Scalar>::value,typename DERIVED::Scalar >::type >\
-	void compute_analytical_gradient(  const Eigen::MatrixBase<DERIVED>& x_variable, Eigen::MatrixBase<DERIVED_2>& gradient_to_compute ) const
+	template< 	typename DERIVED_SIGNATURE_MACRO_q1w2,\
+		   		typename DERIVED_SIGNATURE_MACRO_q1w2_2,\
+		   		typename = typename std::enable_if< DERIVED_SIGNATURE_MACRO_q1w2::ColsAtCompileTime == 1 , DERIVED_SIGNATURE_MACRO_q1w2 >::type,\
+		   		typename = typename std::enable_if< DERIVED_SIGNATURE_MACRO_q1w2_2::ColsAtCompileTime == 1 , DERIVED_SIGNATURE_MACRO_q1w2_2 >::type,\
+				typename = typename std::enable_if< std::is_floating_point<typename DERIVED_SIGNATURE_MACRO_q1w2::Scalar>::value , typename DERIVED_SIGNATURE_MACRO_q1w2::Scalar >::type,\
+				typename = typename std::enable_if< std::is_same<typename DERIVED_SIGNATURE_MACRO_q1w2::Scalar, typename DERIVED_SIGNATURE_MACRO_q1w2_2::Scalar>::value,typename DERIVED_SIGNATURE_MACRO_q1w2::Scalar >::type >\
+	void compute_analytical_gradient(  const Eigen::MatrixBase<DERIVED_SIGNATURE_MACRO_q1w2>& x_variable, Eigen::MatrixBase<DERIVED_SIGNATURE_MACRO_q1w2_2>& gradient_to_compute ) const
 
 
 #define TEMPLATED_SIGNATURE_STOCHASTIC_GRADIENT_FUNCTION(x_variable,gradient_to_compute,idx_to_sample)\
 	template< 	typename IndexType,\
-				typename DERIVED,\
-				typename DERIVED_2,\
-				typename = typename std::enable_if< DERIVED::ColsAtCompileTime == 1 , DERIVED >::type,\
-				typename = typename std::enable_if< DERIVED_2::ColsAtCompileTime == 1 , DERIVED_2 >::type,\
-				typename = typename std::enable_if< std::is_floating_point<typename DERIVED::Scalar>::value , typename DERIVED::Scalar >::type,\
-				typename = typename std::enable_if< std::is_same<typename DERIVED::Scalar, typename DERIVED_2::Scalar>::value,typename DERIVED::Scalar >::type,\
+				typename DERIVED_SIGNATURE_MACRO_q1w2,\
+				typename DERIVED_SIGNATURE_MACRO_q1w2_2,\
+				typename = typename std::enable_if< DERIVED_SIGNATURE_MACRO_q1w2::ColsAtCompileTime == 1 , DERIVED_SIGNATURE_MACRO_q1w2 >::type,\
+				typename = typename std::enable_if< DERIVED_SIGNATURE_MACRO_q1w2_2::ColsAtCompileTime == 1 , DERIVED_SIGNATURE_MACRO_q1w2_2 >::type,\
+				typename = typename std::enable_if< std::is_floating_point<typename DERIVED_SIGNATURE_MACRO_q1w2::Scalar>::value , typename DERIVED_SIGNATURE_MACRO_q1w2::Scalar >::type,\
+				typename = typename std::enable_if< std::is_same<typename DERIVED_SIGNATURE_MACRO_q1w2::Scalar, typename DERIVED_SIGNATURE_MACRO_q1w2_2::Scalar>::value,typename DERIVED_SIGNATURE_MACRO_q1w2::Scalar >::type,\
 				typename = typename std::enable_if< std::is_integral<IndexType>::value && std::is_unsigned<IndexType>::value, void >::type >\
-	void compute_stochastic_gradient( const Eigen::MatrixBase<DERIVED>& x_variable, Eigen::MatrixBase<DERIVED_2>& gradient_to_compute, const MLVector< IndexType >& idx_to_sample ) const
+	void compute_stochastic_gradient( const Eigen::MatrixBase<DERIVED_SIGNATURE_MACRO_q1w2>& x_variable, Eigen::MatrixBase<DERIVED_SIGNATURE_MACRO_q1w2_2>& gradient_to_compute, const MLVector< IndexType >& idx_to_sample ) const
 
 
 namespace MLearn{
@@ -73,7 +73,7 @@ namespace MLearn{
 				   		typename = typename std::enable_if< DERIVED::ColsAtCompileTime == 1 , DERIVED >::type >
 			typename DERIVED::Scalar eval( const Eigen::MatrixBase<DERIVED>& x ) const{
 				MLEARN_FORCED_WARNING_MESSAGE("USING DEFAULT IMPLEMENTATION!");
-				return DERIVED::Scalar(0);
+				return typename DERIVED::Scalar(0);
 			}
 			// -- analytical gradient
 			template< 	typename DERIVED,

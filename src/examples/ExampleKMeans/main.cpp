5bb374bd0b5f50e275eda427b59af606e8f9c776
frenaut <felix.renaut@gmail.com>
2016-10-06 12:35:17 +0200

    added structure for example file

---
+++ src/examples/ExampleKMeans/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include <MLearn/Core>
+#include <MLearn/Clustering/KMeans/KMeans.h>
+
+int main(int argc, char* argv[]){
+
+	using namespace MLearn;
+	using namespace Clustering;
+
+  // TODO(frenaut): add KMeans example
+  return 0;	
+}

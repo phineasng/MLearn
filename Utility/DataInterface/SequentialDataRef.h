61f26df39a9b13e705e35a2ad2532b18419e5c5b
phineasng <nguyen.phineas@gmail.com>
2016-10-14 01:54:26 +0200

    Fixed ref version of sequential data interface

--- Utility/DataInterface/SequentialDataRef.h
+++ Utility/DataInterface/SequentialDataRef.h
@@ -25,10 +25,10 @@ namespace MLearn{
 					output_data_(_output)
 				{}
 				const Eigen::Ref< const MLMatrix<ScalarType> > getInput( IndexType idx ) const{
-					return input_data_.block(0,info(Interface::start_in_idx,idx),input_data_.rows(),info(Interface::n_inputs_idx,idx));
+					return input_data_.block(0,Interface::info(Interface::start_in_idx,idx),input_data_.rows(),Interface::info(Interface::n_inputs_idx,idx));
 				}
 				const Eigen::Ref< const MLMatrix<ScalarType> > getOutput( IndexType idx ) const{
-					return output_data_.block(0,info(Interface::start_out_idx,idx),output_data_.rows(),info(Interface::n_outputs_idx,idx));
+					return output_data_.block(0,Interface::info(Interface::start_out_idx,idx),output_data_.rows(),Interface::info(Interface::n_outputs_idx,idx));
 				}
 			private:
 				const MLMatrix<ScalarType>& input_data_;

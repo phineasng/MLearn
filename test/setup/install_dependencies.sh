127f6c207f1d13b72da396fed4dad24eece84d94
phineasng <nguyen.phineas@gmail.com>
2017-04-22 20:55:33 +0100

    Added travis to repo and first dummy test to verify setup

---
+++ test/setup/install_dependencies.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# get dependencies from the repos
+# - Eigen
+mkdir $TMP_DIR'/Eigen'
+wget https://bitbucket.org/eigen/eigen/get/f3a22f35b044.zip -O $TMP_DIR'/Eigen/eigen_repo.zip'
+unzip $TMP_DIR'/Eigen/eigen_repo.zip' -d $TMP_DIR'/Eigen/'
+mv $TMP_DIR'/Eigen/eigen-eigen-f3a22f35b044/Eigen' /usr/include/Eigen

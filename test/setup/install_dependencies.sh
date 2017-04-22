cb52650d300a2a7d25fcdfe208bf40928a771c21
phineasng <nguyen.phineas@gmail.com>
2017-04-22 21:31:55 +0100

    Some cleaning and trying to include paths to headers

--- test/setup/install_dependencies.sh
+++ test/setup/install_dependencies.sh
@@ -2,13 +2,7 @@
 
 # get dependencies from the repos
 # - Eigen
-echo 'Install Eigen ..'
-echo '.. creating temporary directory to save zip ..'
 mkdir $TMP_DIR'/Eigen'
-echo '.. downloading ..'
 wget https://bitbucket.org/eigen/eigen/get/f3a22f35b044.zip -O $TMP_DIR'/Eigen/eigen_repo.zip'
-echo '.. unzipping ..'
 unzip $TMP_DIR'/Eigen/eigen_repo.zip' -d $TMP_DIR'/Eigen/'
-echo '.. moving to include ..'
-mv $TMP_DIR'/Eigen/eigen-eigen-f3a22f35b044/Eigen' /usr/include/Eigen
-echo '.. done!'
+mv $TMP_DIR'/Eigen/eigen-eigen-f3a22f35b044/Eigen' $INCLUDE_PATH'/Eigen'

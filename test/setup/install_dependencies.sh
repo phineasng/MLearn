df2aa22d8ea299185d723e590cbcaa86af84b528
phineasng <nguyen.phineas@gmail.com>
2017-04-22 21:10:09 +0100

    Added sudo to all lines in install script

--- test/setup/install_dependencies.sh
+++ test/setup/install_dependencies.sh
@@ -2,7 +2,13 @@
 
 # get dependencies from the repos
 # - Eigen
-mkdir $TMP_DIR'/Eigen'
-wget https://bitbucket.org/eigen/eigen/get/f3a22f35b044.zip -O $TMP_DIR'/Eigen/eigen_repo.zip'
-unzip $TMP_DIR'/Eigen/eigen_repo.zip' -d $TMP_DIR'/Eigen/'
+echo 'Install Eigen ..'
+echo '.. creating temporary directory to save zip ..'
+sudo mkdir $TMP_DIR'/Eigen'
+echo '.. downloading ..'
+sudo wget https://bitbucket.org/eigen/eigen/get/f3a22f35b044.zip -O $TMP_DIR'/Eigen/eigen_repo.zip'
+echo '.. unzipping ..'
+sudo unzip $TMP_DIR'/Eigen/eigen_repo.zip' -d $TMP_DIR'/Eigen/'
+echo '.. moving to include ..'
 sudo mv $TMP_DIR'/Eigen/eigen-eigen-f3a22f35b044/Eigen' /usr/include/Eigen
+echo '.. done!'

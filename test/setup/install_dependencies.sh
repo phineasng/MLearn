d85def9f0f798deac6383ae639d5a14b80dd4c6c
phineasng <nguyen.phineas@gmail.com>
2017-04-22 21:15:49 +0100

    Trying to fix the permission issue with travis

--- test/setup/install_dependencies.sh
+++ test/setup/install_dependencies.sh
@@ -4,11 +4,11 @@
 # - Eigen
 echo 'Install Eigen ..'
 echo '.. creating temporary directory to save zip ..'
-sudo mkdir $TMP_DIR'/Eigen'
+mkdir $TMP_DIR'/Eigen'
 echo '.. downloading ..'
-sudo wget https://bitbucket.org/eigen/eigen/get/f3a22f35b044.zip -O $TMP_DIR'/Eigen/eigen_repo.zip'
+wget https://bitbucket.org/eigen/eigen/get/f3a22f35b044.zip -O $TMP_DIR'/Eigen/eigen_repo.zip'
 echo '.. unzipping ..'
-sudo unzip $TMP_DIR'/Eigen/eigen_repo.zip' -d $TMP_DIR'/Eigen/'
+unzip $TMP_DIR'/Eigen/eigen_repo.zip' -d $TMP_DIR'/Eigen/'
 echo '.. moving to include ..'
-sudo mv $TMP_DIR'/Eigen/eigen-eigen-f3a22f35b044/Eigen' /usr/include/Eigen
+mv $TMP_DIR'/Eigen/eigen-eigen-f3a22f35b044/Eigen' /usr/include/Eigen
 echo '.. done!'

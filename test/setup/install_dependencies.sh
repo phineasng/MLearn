cbabeb428a2a01ed7c2b3c99132756bb582258bc
phineasng <nguyen.phineas@gmail.com>
2017-04-22 21:45:15 +0100

    Fixing one more permission error and change compiler

--- test/setup/install_dependencies.sh
+++ test/setup/install_dependencies.sh
@@ -4,5 +4,5 @@
 # - Eigen
 mkdir $TMP_DIR'/Eigen'
 wget https://bitbucket.org/eigen/eigen/get/f3a22f35b044.zip -O $TMP_DIR'/Eigen/eigen_repo.zip'
-unzip $TMP_DIR'/Eigen/eigen_repo.zip' -d $TMP_DIR'/Eigen/'
-sudo mv $TMP_DIR'/Eigen/eigen-eigen-f3a22f35b044/Eigen' $INCLUDE_PATH'/Eigen'
+unzip -qq $TMP_DIR'/Eigen/eigen_repo.zip' -d $TMP_DIR'/Eigen/'
+mv $TMP_DIR'/Eigen/eigen-eigen-f3a22f35b044/Eigen' $INCLUDE_PATH'/Eigen'

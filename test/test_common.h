1fd442fe9221005381ef0c911d1050ef74182c39
phineasng <nguyen.phineas@gmail.com>
2017-04-30 14:08:24 +0100

    Added common test functionality: inf norm of the difference of matrices

--- test/test_common.h
+++ test/test_common.h
@@ -1,1 +1,19 @@
 #define TEST_FLOAT_TOLERANCE 1e-10
+
+// MLearn includes
+#include <MLearn/Core>
+
+// Eigen includes
+#include <Eigen/Core>
+
+namespace TestUtils{
+	/*!
+		\brief compute inf norm of difference of input matrices
+	*/
+	template <typename DERIVED1, typename DERIVED2>
+	typename DERIVED1::Scalar diff_norm(
+			const Eigen::MatrixBase<DERIVED1>& m1,
+			const Eigen::MatrixBase<DERIVED2>& m2){
+		return (m1 - m2).template lpNorm<Eigen::Infinity>();
+	}
+}
